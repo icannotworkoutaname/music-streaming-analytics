ff8dd352e29870f33377c83f6d4a95bd10974d58
CatchThatChocolate <2321225865@qq.com>
2026-07-31 20:43:20 +0000

    delete source change part

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,3 @@
-// 阿里云镜像：国内直连 Gradle Plugin Portal / Maven Central 经常 TLS 握手被重置，改走阿里云镜像加速构建
-pluginManagement {
-    repositories {
-        maven("https://maven.aliyun.com/repository/public")
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        gradlePluginPortal()
-    }
-}
-
 rootProject.name = "music-streaming-analytics"
 
 include(":ingestion-service", ":stream-processor", ":query-service")
